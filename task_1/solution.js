6dfcbb4bab024b0602b0c65fde3726821bca94aa
NadyKalechenok <tiinny@rambler.ru>
2021-10-31 15:43:01 +0300

    task_1 done

--- task_1/solution.js
+++ task_1/solution.js
@@ -1,20 +1,11 @@
 function addInCartMessage(title, price) {
     let productName = title;
     let productPrice = price;
+    let message = productName;
+    message = message + " " + "за" + " " + productPrice + " " + "теперь в корзине!";
+    console.log(message);
 
     // Задание №1.1. Оповещение о добавлении в корзину
-
-    // создайте переменную message
-    // присвойте переменной message значение переменной productName
-    // добавьте в конец переменной message пробел
-    // добавьте в конец переменной message текст 'за'
-    // добавьте в конец переменной message пробел
-    // добавьте в конец переменной message значение переменной productPrice
-    // добавьте в конец переменной message пробел
-    // добавьте в конец переменной message текст 'теперь в корзине!'
-    // выведите переменную message в консоль
-
-    // Конец решения задания №1.1.
 
     return message;
 }
@@ -23,12 +14,8 @@ function addInCartCountChange(value) {
 
     // Задание №1.2. Увеличение количества в корзине
     let oldValue = value;
-
-    // создайте переменную newValue
-    // присвойте ей значение oldValue
-    // увеличьте значение newValue на 1
-
-    // Конец решения задания №1.2.
+    let newValue = oldValue;
+    newValue = newValue + 1;
 
     return newValue;
 }
@@ -36,14 +23,10 @@ function addInCartCountChange(value) {
 function addInCartSumChange(sum, delta) {
     let oldSum = sum;
     let difference = delta;
+    let newSum = oldSum + difference;
+    let newSumText = `${newSum} ₽`;
 
     // Задание №1.3. Увеличение суммы в корзине
-
-    // создайте переменную newSum и присвойте ей сумму oldSum и difference
-    // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
-    // замените склеивание, используемое в newSum Text на шаблонную строку
-
-    // Конец решения задания №1.3.
 
     return newSumText;
 }
